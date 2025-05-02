e80d2b5e7cd126ab9f9b94b50c826d336a8f8c42
Om Mehta <ommehta16@icloud.com>
2025-05-01 20:39:16 -0400

    Some starting content

---
+++ index.js
@@ -0,0 +1,6 @@
+addEventListener("DOMContentLoaded", () => {
+    setTimeout(() => {
+        // document.body.style.transform = "translate(0px,0px)";
+        document.body.style.opacity = 1;
+    }, 100);
+});
