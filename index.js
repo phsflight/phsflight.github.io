9988f7ba180b86c194d998d9dc99cba47c64cb31
Om Mehta <ommehta16@icloud.com>
2025-05-10 22:00:42 -0400

    Start from (mostly) scratch -- basically just top + nav rn lol

--- index.js
+++ index.js
@@ -4,3 +4,21 @@ addEventListener("DOMContentLoaded", () => {
         document.body.style.opacity = 1;
     }, 100);
 });
+
+let pastTop = false;
+
+document.addEventListener("scroll", e => {
+    console.log(document.documentElement.scrollTop);
+    const below = (document.documentElement.scrollTop > window.innerHeight);
+    const topNav = document.getElementsByTagName("nav").item(0);
+    if (document.documentElement.scrollTop > window.innerHeight) {
+        if (pastTop) return;
+        topNav.classList.remove("up")
+        pastTop = true;
+    }
+    else {
+        if (!pastTop) return;
+        topNav.classList.add("up");
+        pastTop = false;
+    }
+})
